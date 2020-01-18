6640781d22da454bfdad8054784d740e1a9e5538
Patrick Norton <patrick.147.norton@gmail.com>
2020-01-17 20:49:55 -0500

    Improved constants

--- Constants.h
+++ Constants.h
@@ -9,8 +9,12 @@
 #include <cstdint>
 
 namespace Constants {  // TODO: Change namespace to not conflict with constant types
-    constexpr const size_t INT_32_BYTES = 32/8;
-    constexpr const size_t INT_16_BYTES = 16/8;
+    constexpr const size_t INT_8_BITS = std::numeric_limits<uint8_t>::digits;
+    constexpr const size_t INT_16_BITS = std::numeric_limits<uint16_t>::digits;
+    constexpr const size_t INT_32_BITS = std::numeric_limits<uint32_t >::digits;
+
+    constexpr const size_t INT_32_BYTES = INT_32_BITS/INT_8_BITS;
+    constexpr const size_t INT_16_BYTES = INT_16_BITS/INT_8_BITS;
     constexpr const uint32_t MAGIC_NUMBER = 0x0ABADE66;
 }
 
