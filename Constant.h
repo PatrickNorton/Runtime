2e379cfd22ea3be01e6a1bc62542a72a980c3b88
Patrick Norton <patrick.147.norton@gmail.com>
2020-02-12 11:26:48 -0500

    Removed unused typedef

--- Constant.h
+++ Constant.h
@@ -10,7 +10,6 @@
 
 namespace Constants {
     typedef void (*NativeCallable)(const std::vector<Variable>&, Runtime*);
-    typedef void (*NativeMethod)(const Variable&, const std::vector<Variable>&, Runtime*);
 
     template <typename T>
     using GenericMethod = void (*)(const std::shared_ptr<T>&, const std::vector<Variable>&, Runtime*);
